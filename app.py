a2a4b6b47b86fa9c9d1a5a0ce63c0bd03d57932d
Chris <stac1221@gmail.com>
2020-07-24 03:51:16 -0500

    Added stock api data. Accepts ticker symbol from Slack and returns current value. TODO: handle getting non-existent ticker symbols

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
+import os
+import requests
+
+
+def is_request_valid(request):
+	is_token_valid = request.form['token'] == os.environ['SLACK_TOKEN']
+	is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+
+	return is_token_valid and is_team_id_valid
+
 
 app = Flask(__name__)
 
 
 @app.route('/stocks', methods=['POST'])
 def stocks():
-	req_data = request.get_json()
-	test = req_data['test']
-	return test
+	if not is_request_valid(request):
+		abort(400)
+
+	if request.form.get('text'):
+		symbol = request.form.get('text', None)
+		apiBaseUrl = 'https://finnhub.io/api/v1/'
+		token = os.environ['FINNHUB_TOKEN']
+		dataType = 'quote'
+		r = requests.get(f'{apiBaseUrl}{dataType}/?symbol={symbol}&token={token}')
+		currentPrice = r.json()["c"]
+
+		msg = f'{symbol} — Current price: ${currentPrice}'
+	else:
+		msg = 'Please follow command with a valid ticker symbol'
+
+	return jsonify(
+		response_type='in_channel',
+		text=msg,
+	)
 
 
 @app.route('/')
